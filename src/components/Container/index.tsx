23c7d6aa7277d88310efb56369e674c4d73efa14
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-20 16:33:07 -0300

    Aula dia 20/01

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export function Container({ children }: { children: React.ReactNode }) {
+  return <div className={styles['container-fluid']}>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
+  </div>;
+}
