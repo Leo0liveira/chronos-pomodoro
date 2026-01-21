8064618af97f53cff4a6d55e55aa724107165f14
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-21 17:39:02 -0300

    Organizando o contexto em arquivos separados

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,25 +2,10 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { Input } from "../DefaultInput";
-import { useTaskContext } from "../../contexts/TaskContext";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-
-  function handleClick() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: "21:00",
-      };
-    });
-  }
-
   return (
     <form className="form" action="">
-      <button onClick={handleClick} type="button">
-        Clicar
-      </button>
       <div className="formRow">
         <Input
           labelText="task"
