c7b4fd4eb5a0ae660d1b90172e46ad8e196c1656
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-21 09:20:54 -0300

    Criando troca de temas

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,54 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
-import styles from './styles.module.css';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function toggleTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  }
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
-        <HouseIcon/> 
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a Home"
+        title="Ir para a Home"
+      >
+        <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <HistoryIcon/> 
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para o Histórico"
+        title="Ir para o Histórico"
+      >
+        <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <SettingsIcon/> 
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para as Configurações"
+        title="Ir para as Configurações"
+      >
+        <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <SunIcon/> 
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Trocar Tema"
+        title="Trocar Tema"
+        onClick={toggleTheme}
+      >
+        <SunIcon />
       </a>
     </nav>
-  )
+  );
 }
