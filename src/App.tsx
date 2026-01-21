49f99635cc99b8656f994eae961971f842b4f046
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-21 10:31:45 -0300

    Criando a página Home e um template para todas as outras páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,8 @@
-import { Container } from './components/Container/';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import Input from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from "./pages/Home";
 
-import './styles/theme.css';
-import './styles/global.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
-  return(
-    <>
-    <Container>
-      <Logo/>
-    </Container>
-    <Container>
-      <Menu/>
-    </Container>
-    <Container>
-      <CountDown/>
-    </Container>
-    <Container>
-      <form action="" className="form">
-        <div className="formRow">
-          <Input 
-            labelText="Vou trabalhar em:" 
-            id="task" 
-            type='text' 
-            placeholder='Digite algo'/>
-        </div>
-        <div className="formRow">
-          <span> Nesse ciclo
-          <strong> descanse </strong>
-          <span> por </span>
-          <strong> 5 minutos. </strong>
-          </span>
-        </div>
-        <div className="formRow">
-          <Cycles/>
-        </div>
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,15 +1,15 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 type InputProps = {
-    id: string;
-    labelText?: string;
-} & React.ComponentProps<'input'>
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<"input">;
 
-export default function Input( { type, id, labelText, ...rest }: InputProps) { 
-    return (
-        <>
-            {labelText && <label htmlFor={id}>{labelText}</label>}
-            <input className={styles.input} type={type} id={id} {...rest}/>
-        </>
-    )
+export function Input({ type, id, labelText, ...rest }: InputProps) {
+  return (
+    <>
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input className={styles.input} type={type} id={id} {...rest} />
+    </>
+  );
 }
