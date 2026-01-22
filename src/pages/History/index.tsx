ffc917a190471a35b9c522593f846fc48a9e5162
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-22 15:36:16 -0300

    Formatando a data com date-fns

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -3,6 +3,7 @@ import { Container } from "../../components/Container";
 import { DefaultButton } from "../../components/DefaultButton";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
+import { formatDate } from "../../utils/formatDate";
 
 import styles from "./styles.module.css";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
@@ -45,7 +46,7 @@ export function History() {
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
-                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{formatDate(task.startDate)}</td>
                     <td>{task.interruptDate}</td>
                     <td>{task.type}</td>
                   </tr>
