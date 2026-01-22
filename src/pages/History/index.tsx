f55035b44d26fe1be295da25d20a24b042c292c3
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-22 15:38:19 -0300

    Lógica para o status da tarefa - Em progresso, Completa, Interrompida ou Abandonada

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,6 +4,7 @@ import { DefaultButton } from "../../components/DefaultButton";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
 import { formatDate } from "../../utils/formatDate";
+import { getTaskStatus } from "../../utils/getTaskStatus";
 
 import styles from "./styles.module.css";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
@@ -47,7 +48,7 @@ export function History() {
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
                     <td>{formatDate(task.startDate)}</td>
-                    <td>{task.interruptDate}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
                     <td>{task.type}</td>
                   </tr>
                 );
