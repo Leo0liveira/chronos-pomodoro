8d58afdf1f1ffcee2119c9aef588c5427d1c4197
Leonardo Oliveira <lfofreitas@gmail.com>
2026-01-22 14:58:36 -0300

    Criando um router e o componente ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { Home } from "./pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { MessagesContainer } from "./components/MessagesContainer";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { NotFound } from "./pages/NotFound";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
+import { MainRouter } from "./routers/MainRouter";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -12,13 +9,7 @@ export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
